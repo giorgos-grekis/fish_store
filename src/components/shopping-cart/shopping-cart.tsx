09e22fa855f23a3f740883f53fb44876bf1674b3
cisu <georgrekis@gmail.com>
2024-04-30 23:16:28 +0300

    create a base template shopping cart

--- src/components/shopping-cart/shopping-cart.tsx
+++ src/components/shopping-cart/shopping-cart.tsx
@@ -1,18 +1,42 @@
-import React from "react";
+import styles from "./shopping-cart.module.scss";
 
 const ShoppingCart = () => {
   return (
     <div className="col inner-border">
       {/* Shopping Cart */}
       <div className="">
-        <h4>SHOPPING CART</h4>
-        <ul className="">
+        <h3 className={`${styles.title}`}>SHOPPING CART</h3>
+
+        <ul className={`${styles.listCartItems}`}>
           <li className="">
-            Pacific Halibut
-            <span>$17.24</span>
+            <div>
+              <span className={`${styles.qty}`}>13</span>
+              <span style={{ margin: "0 0.3rem" }}>x</span>
+              <span>Pacific Halibut</span>
+            </div>
+            <div className={`${styles.price}`}>$17.24</div>
+          </li>
+
+          <li className="">
+            <div>
+              <span className={`${styles.qty}`}>13</span>
+              <span style={{ margin: "0 0.3rem" }}>x</span>
+              <span>Pacific Halibut</span>
+            </div>
+            <div className={`${styles.price}`}>$17.24</div>
+          </li>
+
+          <li className="">
+            <div>
+              <span className={`${styles.qty}`}>13</span>
+              <span style={{ margin: "0 0.3rem" }}>x</span>
+              <span>Pacific Halibut</span>
+            </div>
+            <div className={`${styles.price}`}>$17.24</div>
           </li>
         </ul>
-        <div className="">
+
+        <div className={`${styles.totals}`}>
           <h5>Total: $17.24</h5>
         </div>
       </div>
