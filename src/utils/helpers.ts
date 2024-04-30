a6f2485a83949943e093dff68389a63ef2974080
cisu <georgrekis@gmail.com>
2024-05-01 00:31:02 +0300

    create some helper formated  price function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,8 +1,3 @@
 export const formatedPrice = (price: number): string => {
-  //   return (price / 100).toLocaleString("en-US", {
-  //     style: "currency",
-  //     currency: "USD",
-  //   });
-
   return (price / 100).toFixed(2);
 };
