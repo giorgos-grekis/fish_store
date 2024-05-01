65cedbb1859cc6104bbb233bcffaab758939d079
cisu <georgrekis@gmail.com>
2024-05-01 13:51:28 +0300

    successful upload image

---
+++ src/app/api/upload/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse, NextRequest } from "next/server";
+import { writeFile } from "fs/promises";
+import path from "path";
+
+export async function POST(request: NextRequest) {
+  const url = new URL(request.url);
+  const searchParams = new URLSearchParams(url.searchParams);
+
+  const id = searchParams.get("id");
+  //   const title = searchParams.get("title");
+
+  const data = await request.formData();
+  const file: File | null = data.get("image") as unknown as File;
+
+  if (!file) {
+    return NextResponse.json({ success: false });
+  }
+
+  const fileExtinction = file.type?.split("/")?.[1];
+
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+
+  const pathname = path.join(
+    process.cwd(),
+    `/public/images/${id}.${fileExtinction}`
+  );
+
+  await writeFile(pathname, buffer);
+  return NextResponse.json({ success: true });
+}
