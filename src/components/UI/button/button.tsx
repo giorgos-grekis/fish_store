5d8bdcc577478777de0880abdce22f87f1c3d9ea
cisu <georgrekis@gmail.com>
2024-05-01 10:56:20 +0300

    worked with functionality for the button

--- src/components/UI/button/button.tsx
+++ src/components/UI/button/button.tsx
@@ -1,14 +1,43 @@
+"use client";
+import { FishType } from "@/components/fish-store/card/card";
 import styles from "./button.module.scss";
+import { json } from "stream/consumers";
 
 interface ButtonType extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   content: string;
   className?: string;
+  fish?: FishType;
 }
 //  HTMLButtonElement
 
-const Button = ({ content, className, type = "button" }: ButtonType) => {
+const Button = ({ fish, content, className, type = "button" }: ButtonType) => {
+  console.log("fish button: ", fish);
+
+  const handleOnClick = () => {
+    if (type !== "button" && !fish) return;
+
+    const fishStoreCart = localStorage.getItem("fish_store_cart");
+    if (!fishStoreCart) {
+      localStorage.setItem("fish_store_cart", JSON.stringify([fish]));
+      return;
+    }
+
+    const fishStoreCartAdd = JSON.parse(
+      localStorage.getItem("fish_store_cart") ?? ""
+    );
+    fishStoreCartAdd.push(fish);
+
+    localStorage.setItem("fish_store_cart", JSON.stringify(fishStoreCartAdd));
+
+    console.log("fishStoreCart test: ", fishStoreCartAdd);
+  };
+
   return (
-    <button className={`${styles.btn} ${className}`} type={type}>
+    <button
+      onClick={handleOnClick}
+      className={`${styles.btn} ${className}`}
+      type={type}
+    >
       <>{content}</>
     </button>
   );
