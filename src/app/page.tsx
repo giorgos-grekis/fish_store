8b9411ecc68add2d2563a0518dbc1e224ae61d4e
cisu <georgrekis@gmail.com>
2024-05-01 10:56:54 +0300

    removed an extra div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,16 @@
 import FishStore from "@/components/fish-store/fish-store";
 import ShoppingCart from "@/components/shopping-cart/shopping-cart";
 import Inventory from "@/components/inventory/inventory";
-import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className="container grid-container my-3">
-        {/* FISH STORE */}
-        <FishStore />
-        {/* SHOPPING CART */}
-        <ShoppingCart />
-        {/* INVENTORY */}
-        <Inventory />
-      </div>
+    <main className="container grid-container my-3">
+      {/* FISH STORE */}
+      <FishStore />
+      {/* SHOPPING CART */}
+      <ShoppingCart />
+      {/* INVENTORY */}
+      <Inventory />
     </main>
   );
 }
