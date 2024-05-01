44361200eedba5c2bedf2189ac3ed3013af71c20
cisu <georgrekis@gmail.com>
2024-05-01 10:55:02 +0300

    add use client to shopping-cart component

--- src/components/shopping-cart/shopping-cart.tsx
+++ src/components/shopping-cart/shopping-cart.tsx
@@ -1,6 +1,22 @@
+"use client";
+import { useEffect } from "react";
 import styles from "./shopping-cart.module.scss";
+import { formatedPrice } from "@/utils/helpers";
+import type { FishType } from "../fish-store/card/card";
+
+const findTotalPrice = (items: FishType[]) => {
+  let price = 0;
+  for (const fish of items) {
+    price += fish.price;
+  }
+  return price;
+};
 
 const ShoppingCart = () => {
+  const items = JSON.parse(localStorage.getItem("fish_store_cart") ?? "");
+
+  console.log("ShoppingCart items: ", items);
+
   return (
     <section className="col inner-border">
       {/* Shopping Cart */}
@@ -8,36 +24,22 @@ const ShoppingCart = () => {
         <h3 className={`${styles.title}`}>SHOPPING CART</h3>
 
         <ul className={`${styles.listCartItems}`}>
-          <li className="">
-            <div>
-              <span className={`${styles.qty}`}>13</span>
-              <span style={{ margin: "0 0.3rem" }}>x</span>
-              <span>Pacific Halibut</span>
-            </div>
-            <div className={`${styles.price}`}>$17.24</div>
-          </li>
-
-          <li className="">
-            <div>
-              <span className={`${styles.qty}`}>13</span>
-              <span style={{ margin: "0 0.3rem" }}>x</span>
-              <span>Pacific Halibut</span>
-            </div>
-            <div className={`${styles.price}`}>$17.24</div>
-          </li>
-
-          <li className="">
-            <div>
-              <span className={`${styles.qty}`}>13</span>
-              <span style={{ margin: "0 0.3rem" }}>x</span>
-              <span>Pacific Halibut</span>
-            </div>
-            <div className={`${styles.price}`}>$17.24</div>
-          </li>
+          {(items ?? []).map((item: FishType) => (
+            <li className="" key={item.id}>
+              <div>
+                <span className={`${styles.qty}`}>13</span>
+                <span style={{ margin: "0 0.3rem" }}>x</span>
+                <span>{item.name}</span>
+              </div>
+              <div className={`${styles.price}`}>
+                ${formatedPrice(item.price)}
+              </div>
+            </li>
+          ))}
         </ul>
 
         <div className={`${styles.totals}`}>
-          <h4>Total: $17.24</h4>
+          <h4>Total: ${formatedPrice(findTotalPrice(items))}</h4>
         </div>
       </div>
     </section>
